bd8d339322e7388e45caf989cc963066f41fd25f
Chris Chou <chhschou@gmail.com>
2017-08-30 11:28:59 +1200

    fix submit event and add date

--- client/container/EventPage.jsx
+++ client/container/EventPage.jsx
@@ -1,29 +1,28 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {Grid, Row, Col} from 'react-bootstrap'
-import {createEvent} from '../actions/events'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Grid, Row, Col, FormGroup, FormControl } from 'react-bootstrap'
+import { createEvent } from '../actions/events'
 import Logo from '../components/Logo'
 
 export class EventPage extends React.Component {
   constructor(props) {
     super(props)
+    const now = new Date()
+    const dateString = `${now.getFullYear()}-${String('0' + (now.getMonth() + 1)).slice(-2)}-${now.getDate()}`
+
     this.state = {
       title: '',
       description: '',
       user_id: null,
-      date: new Date().getTime() / 1000,
+      date: dateString,
       is_am: true,
     }
   }
 
   onChange(e) {
-    let value = e.target.value
-    if (e.target.name === 'date')
-      value = new Date(value)
-
     this.setState({
-      [e.target.name]: value
+      [e.target.name]: e.target.value
     })
   }
 
@@ -31,32 +30,32 @@ export class EventPage extends React.Component {
     e.preventDefault()
     const newEvent = { ...this.state }
     newEvent.user_id = this.props.user.id
-    this.props.dispatch(createEvent(newEvent))
+    this.props.dispatch(createEvent(newEvent), this.props.history.push('/eventlist'))
   }
 
   render() {
     return (
       <div className='event'>
-        <Logo/>
+        <Logo />
         <Grid>
           <h2>Start a sizzle</h2>
           <form onSubmit={this.onSubmit.bind(this)}>
             <Row>
-              <input type='text' name='title' placeholder='Please enter title' onChange={this.onChange.bind(this)}/>
+              <input type='text' name='title' placeholder='Please enter title' onChange={this.onChange.bind(this)} />
             </Row>
             <Row>
-              <input type='text' name='description' placeholder='Please enter description' onChange={this.onChange.bind(this)}/>
+              <input type='text' name='description' placeholder='Please enter description' onChange={this.onChange.bind(this)} />
             </Row>
             <Row>
-              <input type="text" name='date' placeholder='DD/MMM/YYYY' onChange={this.onChange.bind(this)}/>
+              <input type="date" name='date' value={this.state.date} onChange={this.onChange.bind(this)} />
               <select name='is_am' onChange={this.onChange.bind(this)}>
                 <option value='true'>AM</option>
                 <option value='false'>PM</option>
               </select>
             </Row>
             <Row>
-              <Link to='EventList'>
-                <button type='submit'>Sizzle!</button>
+              <Link to='/EventList'>
+                <button type='button' onClick={this.onSubmit.bind(this)}>Sizzle!</button>
               </Link>
             </Row>
           </form>
