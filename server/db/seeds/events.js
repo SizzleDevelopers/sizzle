bce42c3fe1edf5b76b03163a5041799c03704967
carla <carlacfw@gmail.com>
2017-08-30 19:25:36 +1200

    seeds repopulated

--- server/db/seeds/events.js
+++ server/db/seeds/events.js
@@ -14,16 +14,30 @@ exports.seed = function(knex, Promise) {
         id: 2,
         date: new Date(2017, 9, 2),
         is_am: false,
-        description: 'cancer society fundraiser',
+        description: 'Cancer society fundraiser',
         title: 'Fundraise for Cancer',
         user_id: 2
       }, {
         id: 3,
         date: new Date(2017, 9, 8),
         is_am: true,
-        description: 'kaibosh fundraiser',
+        description: 'Kaibosh fundraiser',
         title: 'Yet another fundraise',
         user_id: 3
+      }, {
+        id: 4,
+        date: new Date(2017, 10, 1),
+        is_am: true,
+        description: 'Get to know your neighbors at Karori Park fair',
+        title: 'Neighborhood BBQ',
+        user_id: 4
+      }, {
+        id: 5,
+        date: new Date(2017, 9, 28),
+        is_am: true,
+        description: "Let\'s help warm up the less fortunate",
+        title: 'Soup for everyone',
+        user_id: 5
       }
     ]);
   });
