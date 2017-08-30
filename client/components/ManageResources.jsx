9514728995335214d0018bcfb0bd3420b133d947
Chris Chou <chhschou@gmail.com>
2017-08-30 17:58:38 +1200

    refactor ManageResources.jsx  to use Resource.jsx

--- client/components/ManageResources.jsx
+++ client/components/ManageResources.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Grid, Row, Col, Input, Button} from 'react-bootstrap'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import { Grid, Row, Col, Input, Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { getResources } from '../actions/manageResource'
-import {deleteResource} from '../actions/manageResource'
+import Resource from './Resource'
 
 export class ManageResources extends React.Component {
   constructor(props) {
@@ -13,35 +13,17 @@ export class ManageResources extends React.Component {
   componentDidMount() {
     this.props.dispatch(getResources())
   }
-  onDeleteLinkClicked(e) {
-    const resource = this.props.resources.find((resource) => resource.id == e.target.id)
-    this.props.dispatch(deleteResource(resource))
-  }
 
   render() {
     // let resources = this.props.resources
     return (
-      <div>
+      <div className='manage-resources'>
         <Grid>
           <Row>
-            <Col xs={1} md={4}></Col>
-            <Col xs={4} md={4}>
-              <h1>Manage Resources</h1>
-            </Col>
-            <Col xs={1} md={4}></Col>
+            <h1>Manage Resources</h1>
           </Row>
           {this.props.resources.map((resource, i) => {
-            return (
-              <div key={i}>
-                <Row>
-                  <h1>{resource.title}</h1>
-                </Row>
-                <Row>
-                  <p>{resource.type}</p>
-                  <a id={resource.id} onClick={this.onDeleteLinkClicked.bind(this)}>Delete</a>
-                </Row>
-              </div>
-            )
+            return (<Resource key={i} resource={resource} />)
           })}
           <Link to='/MainPage'>
             <button type="button" className="btn btn-primary">Home</button>
@@ -53,7 +35,7 @@ export class ManageResources extends React.Component {
 }
 
 function mapStateToProps(state) {
-  return {resources: state.resources}
+  return { resources: state.resources }
 }
 
 export default connect(mapStateToProps)(ManageResources)
---
+++ client/components/Resource.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Grid, Row, Col, Panel, Button } from 'react-bootstrap'
+import { deleteResource } from '../actions/manageResource'
+
+
+export class Event extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  onDeleteLinkClicked(e) {
+    this.props.dispatch(deleteResource(this.props.resource))
+  }
+
+  render() {
+    let resource = this.props.resource
+    return (
+      <Row>
+        <Panel className='event'>
+          <Grid>
+            <h3>{resource.title}</h3>
+            <p><i>{resource.type}</i></p>
+            <Row>
+              <div className='text-right'>
+                <Col>
+                  <Button style={{ marginRight: '3rem' }} bsStyle='danger' onClick={this.onDeleteLinkClicked.bind(this)}>Delete</Button>
+                </Col>
+              </div>
+            </Row>
+          </Grid>
+        </Panel>
+      </Row>
+    )
+  }
+}
+export default connect()(Event)
+
